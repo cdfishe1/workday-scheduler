6cc1880647ca4aab56ebeaf7589b08bb35f65185
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-03-03 11:24:41 -0600

    Construct forEach to change all input bgcolor.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,21 +4,23 @@ const variables = {
     hour: Number(moment().format('hh')),
     time11am: $('#time11am'),
     time1pm: $('#time1pm'),
+    
+    
 };
 
 variables.currentDay.text(variables.today.format('dddd, MMMM Do YYYY'));
 
+const allInputs = document.getElementsByClassName('form-control');
+const inputsArray = Array.from(allInputs);
 
-const timeColor = () => {
-    let parse = Number(moment().format(time1pm.dataset.time));
-    console.log(typeof parse);
-    console.log(typeof variables.hour);
-    console.log(parse > variables.hour);
-    if (parse > variables.hour) {
-        time1pm.style.backgroundColor = 'green';
-        time1pm.style.color = 'white';
+
+inputsArray.forEach((input) => {
+    if(input.dataset.time < variables.hour) {
+        input.style.backgroundColor = '#d3d3d3'
+    } else if (input.dataset.time > variables.hour) {
+        input.style.backgroundColor = '#77dd77';
     } else {
-        console.log('error');
+        input.style.backgroundColor = '#ff6961'
     }
-};
-timeColor();
+});
+
