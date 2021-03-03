833b7db108a6e779be4cbdf2fcefa318abcf4eda
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-03-03 10:43:05 -0600

    Create function to change bgcolor for one time.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,24 @@
 const variables = {
     currentDay: $('#currentDay'),
     today: moment(),
+    hour: Number(moment().format('hh')),
+    time11am: $('#time11am'),
+    time1pm: $('#time1pm'),
 };
 
 variables.currentDay.text(variables.today.format('dddd, MMMM Do YYYY'));
 
+
+const timeColor = () => {
+    let parse = Number(moment().format(time1pm.dataset.time));
+    console.log(typeof parse);
+    console.log(typeof variables.hour);
+    console.log(parse > variables.hour);
+    if (parse > variables.hour) {
+        time1pm.style.backgroundColor = 'green';
+        time1pm.style.color = 'white';
+    } else {
+        console.log('error');
+    }
+};
+timeColor();
