8448cd447af5fe5407b6637df99d29a131c39d91
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-03-03 15:12:10 -0600

    Add event listner to 9am.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,26 +1,28 @@
 const variables = {
     currentDay: $('#currentDay'),
     today: moment(),
-    hour: Number(moment().format('hh')),
-    time11am: $('#time11am'),
-    time1pm: $('#time1pm'),
-    
-    
+    hour: Number(moment().format('HH')),
+    allInputs: document.getElementsByClassName('form-control'),
+    btn9am: $('btn9am'),
+    time9am: $('time9am'),
 };
 
 variables.currentDay.text(variables.today.format('dddd, MMMM Do YYYY'));
 
-const allInputs = document.getElementsByClassName('form-control');
-const inputsArray = Array.from(allInputs);
-
-
+let inputsArray = Array.from(variables.allInputs);
 inputsArray.forEach((input) => {
     if(input.dataset.time < variables.hour) {
-        input.style.backgroundColor = '#d3d3d3'
-    } else if (input.dataset.time > variables.hour) {
-        input.style.backgroundColor = '#77dd77';
-    } else {
         input.style.backgroundColor = '#ff6961'
+    } else if (input.dataset.time > variables.hour) {
+        input.style.backgroundColor = '#d3d3d3';
+    } else {
+        input.style.backgroundColor = '#77dd77'
     }
 });
+
+variables.btn9am.addEventListner('click', function() {
+    let btn9amEvent = variables.time9am.value;
+    console.log(btn9amEvent);
+});
+
 
