8e526c21c1c33f38ab358c37d2edfb18af874533
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-03-07 15:44:51 -0600

    Add forEach over each button listener.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -31,10 +31,10 @@ variables.currentDay.text(variables.today.format('dddd, MMMM Do YYYY'));
 
 // Creates an array out of the input elements
 let inputsArray = Array.from(variables.allInputs);
-console.log(inputsArray);
+
 // Creates an array of the button elements
 let buttonsArray = Array.from(variables.allButtons);
-console.log(buttonsArray);
+
 
 
 const setDataAttribute = () => {
@@ -51,8 +51,6 @@ setDataAttribute();
 inputsArray.forEach((input) => {
     
     let parsedInputTime = parseInt(input.dataset.hour, 10);
-    console.log(input.dataset.hour);
-    console.log(variables.hour);
     if((parsedInputTime) < variables.hour) {
         input.style.backgroundColor = '#ff6961';
     } else if (parsedInputTime > variables.hour) {
@@ -65,105 +63,112 @@ inputsArray.forEach((input) => {
 
     input.setAttribute('placeholder', 'Enter an event.');
     
-
-    if (localStorage.getItem('event9am') !== null) {
-        let eventText9am = localStorage.getItem('event9am');
-        time9am.value = eventText9am;
-    } 
-
-    if (localStorage.getItem('event10am') !== null) {
-        let eventText10am = localStorage.getItem('event10am');
-        time10am.value = eventText10am;
-    } 
-
-    if (localStorage.getItem('event11am') !== null) {
-        let eventText11am = localStorage.getItem('event11am');
-        time11am.value = eventText11am;
-    } 
-
-    if (localStorage.getItem('event12pm') !== null) {
-        let eventText12pm = localStorage.getItem('event12pm');
-        time12pm.value = eventText12pm;
-    } 
-
-    if (localStorage.getItem('event1pm') !== null) {
-        let eventText1pm = localStorage.getItem('event1pm');
-        time1pm.value = eventText1pm;
-    } 
-
-    if (localStorage.getItem('event2pm') !== null) {
-        let eventText2pm = localStorage.getItem('event2pm');
-        time2pm.value = eventText2pm;
-    } 
-
-    if (localStorage.getItem('event3pm') !== null) {
-        let eventText3pm = localStorage.getItem('event3pm');
-        time3pm.value = eventText3pm;
-    } 
-
-    if (localStorage.getItem('event4pm') !== null) {
-        let eventText4pm = localStorage.getItem('event4pm');
-        time4pm.value = eventText4pm;
-    } 
-
-    if (localStorage.getItem('event5pm') !== null) {
-        let eventText5pm = localStorage.getItem('event5pm');
-        time5pm.value = eventText5pm;
-    } 
     
 });
 
+if (localStorage.getItem('event9am') !== null) {
+    let eventText9am = localStorage.getItem('event9am');
+    time9am.value = eventText9am;
+} 
+
+if (localStorage.getItem('event10am') !== null) {
+    let eventText10am = localStorage.getItem('event10am');
+    time10am.value = eventText10am;
+} 
+
+if (localStorage.getItem('event11am') !== null) {
+    let eventText11am = localStorage.getItem('event11am');
+    time11am.value = eventText11am;
+} 
+
+if (localStorage.getItem('event12pm') !== null) {
+    let eventText12pm = localStorage.getItem('event12pm');
+    time12pm.value = eventText12pm;
+} 
+
+if (localStorage.getItem('event1pm') !== null) {
+    let eventText1pm = localStorage.getItem('event1pm');
+    time1pm.value = eventText1pm;
+} 
+
+if (localStorage.getItem('event2pm') !== null) {
+    let eventText2pm = localStorage.getItem('event2pm');
+    time2pm.value = eventText2pm;
+} 
+
+if (localStorage.getItem('event3pm') !== null) {
+    let eventText3pm = localStorage.getItem('event3pm');
+    time3pm.value = eventText3pm;
+} 
+
+if (localStorage.getItem('event4pm') !== null) {
+    let eventText4pm = localStorage.getItem('event4pm');
+    time4pm.value = eventText4pm;
+} 
+
+if (localStorage.getItem('event5pm') !== null) {
+    let eventText5pm = localStorage.getItem('event5pm');
+    time5pm.value = eventText5pm;
+} 
 
 
 
+buttonsArray.forEach((button) => {
+    button.addEventListener('click', function() {
+        let inputValue = $(this).closest("div.input-group").find("input").val();
+        let timeKey = $(this).closest("div.input-group").find("input").attr("data-hour");
+        localStorage.setItem(timeKey, inputValue);
+    });
+
+});
 
 
-// Sets eventlisteners for each button to add content to local storage
-variables.btn9am.addEventListener('click', function() {
-    let inputText9am = variables.time9am.value;
-    localStorage.setItem('event9am', inputText9am);
-})
+// // Sets eventlisteners for each button to add content to local storage
+// variables.btn9am.addEventListener('click', function() {
+//     let inputText9am = variables.time9am.value;
+//     localStorage.setItem('event9am', inputText9am);
+// })
 
-variables.btn10am.addEventListener('click', function() {
-    let inputText10am = variables.time10am.value;
-    localStorage.setItem('event10am', inputText10am);
-})
+// variables.btn10am.addEventListener('click', function() {
+//     let inputText10am = variables.time10am.value;
+//     localStorage.setItem('event10am', inputText10am);
+// })
 
 
-variables.btn11am.addEventListener('click', function() {
-    let inputText11am = variables.time11am.value;
-    localStorage.setItem('event11am', inputText11am);
-})
+// variables.btn11am.addEventListener('click', function() {
+//     let inputText11am = variables.time11am.value;
+//     localStorage.setItem('event11am', inputText11am);
+// })
 
-variables.btn12pm.addEventListener('click', function() {
-    let inputText12pm = variables.time12pm.value;
-    localStorage.setItem('event12pm', inputText12pm);
-})
+// variables.btn12pm.addEventListener('click', function() {
+//     let inputText12pm = variables.time12pm.value;
+//     localStorage.setItem('event12pm', inputText12pm);
+// })
 
-variables.btn1pm.addEventListener('click', function() {
-    let inputText1pm = variables.time1pm.value;
-    localStorage.setItem('event1pm', inputText1pm);
-})
+// variables.btn1pm.addEventListener('click', function() {
+//     let inputText1pm = variables.time1pm.value;
+//     localStorage.setItem('event1pm', inputText1pm);
+// })
 
-variables.btn2pm.addEventListener('click', function() {
-    let inputText2pm = variables.time2pm.value;
-    localStorage.setItem('event2pm', inputText2pm);
-})
+// variables.btn2pm.addEventListener('click', function() {
+//     let inputText2pm = variables.time2pm.value;
+//     localStorage.setItem('event2pm', inputText2pm);
+// })
 
-variables.btn3pm.addEventListener('click', function() {
-    let inputText3pm = variables.time3pm.value;
-    localStorage.setItem('event3pm', inputText3pm);
-})
+// variables.btn3pm.addEventListener('click', function() {
+//     let inputText3pm = variables.time3pm.value;
+//     localStorage.setItem('event3pm', inputText3pm);
+// })
 
-variables.btn4pm.addEventListener('click', function() {
-    let inputText4pm = variables.time4pm.value;
-    localStorage.setItem('event4pm', inputText4pm);
-})
+// variables.btn4pm.addEventListener('click', function() {
+//     let inputText4pm = variables.time4pm.value;
+//     localStorage.setItem('event4pm', inputText4pm);
+// })
 
-variables.btn5pm.addEventListener('click', function() {
-    let inputText5pm = variables.time5pm.value;
-    localStorage.setItem('event5pm', inputText5pm);
-})
+// variables.btn5pm.addEventListener('click', function() {
+//     let inputText5pm = variables.time5pm.value;
+//     localStorage.setItem('event5pm', inputText5pm);
+// })
 
 
 
